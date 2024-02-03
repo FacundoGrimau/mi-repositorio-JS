9acd82e44b667282ff414f29e0b9f35680e8a974
Facundo Grimau <grimauf@gmail.com>
2024-02-03 11:38:50 -0300

    mi repositorio

---
+++ js/miarchivo.js
@@ -0,0 +1,34 @@
+// Datos para ingresar al sitio:
+let ingresoDeNombre = prompt("Ingresa tu nombre");
+let ingresoDeApellido = prompt("Ingresar tu apellido");
+const EDAD = parseInt(prompt("Ingresar Edad"));
+
+function saludo(nombre, apellido){
+    alert("Bienvenido/a Bonchi Burger " + ingresoDeNombre + " " + ingresoDeApellido + " ¿Cómo estás?");
+    console.log("El nombre completo del usuario es: " + nombre + " " + apellido);
+}
+
+function comprobarEdad(){
+    if(EDAD >= 18){
+       return("Es mayor de edad");
+    }else {
+        return("No es mayor de edad"); 
+    }
+}
+
+function mostrar(mensaje){
+    console.log(mensaje);
+}
+
+saludo(ingresoDeNombre, ingresoDeApellido);
+resultadoEdad = comprobarEdad();
+mostrar(resultadoEdad);
+
+
+// Ciclo a utilizar para los productos del menu con tope máximo :
+for (let productos = 0; productos <= 5; productos++) {
+    if (productos === 6){
+        break;
+    }
+    console.log(productos);
+}
